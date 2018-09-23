2db253fea82ab6aad066b81ddf010e6ac7c768d3
user143604 <user143604@87-237-62-5.rdns.melbourne.co.uk>
2018-09-23 09:06:01 -0700

    Initial Commit

---
+++ MyPlaces/FirstViewController.swift
@@ -0,0 +1,20 @@
+//
+//  FirstViewController.swift
+//  MyPlaces
+//
+//  Created by user143604 on 9/23/18.
+//  Copyright © 2018 user143604. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
---
+++ MyPlaces/SecondViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SecondViewController.swift
+//  MyPlaces
+//
+//  Created by user143604 on 9/23/18.
+//  Copyright © 2018 user143604. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
